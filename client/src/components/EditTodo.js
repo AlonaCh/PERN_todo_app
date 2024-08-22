f02f89d653f08c8a7569788d1e7cd3ecc7e275fa
Alona Chubenko <aliona.chubenko@gmail.com>
2024-08-22 09:36:55 +0300

    Add layout for edit todo

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,9 +1,78 @@
 import React from 'react'
 
+
 const EditTodo = () => {
-  return (
-    <div>EditTodo</div>
-  )
+   return (
+       <>
+           {/* Button to open the modal */}
+           <button
+               type="button"
+               class="btn btn-warning"
+               data-toggle="modal"
+
+
+           >
+               Edit
+           </button>
+
+
+
+
+           <div
+               class="modal"
+           // id={`id${todo.todo_id}`}
+           // onClick={() => setDescription(todo.description)}
+           >
+               <div class="modal-dialog">
+                   <div class="modal-content">
+                       <div class="modal-header">
+                           <h4 class="modal-title">Edit Todo</h4>
+                           <button
+                               type="button"
+                               class="close"
+                               data-dismiss="modal"
+                           // onClick={() => setDescription(todo.description)}
+                           >
+
+
+                           </button>
+                       </div>
+
+
+                       <div class="modal-body">
+                           <input
+                               type="text"
+                               className="form-control"
+                           // value={description}
+                           // onChange={e => setDescription(e.target.value)}
+                           />
+                       </div>
+
+
+                       <div class="modal-footer">
+                           <button
+                               type="button"
+                               class="btn btn-warning"
+                               data-dismiss="modal"
+                           // onClick={e => updateDescription(e)}
+                           >
+                               Edit
+                           </button>
+                           <button
+                               type="button"
+                               class="btn btn-danger"
+                               data-dismiss="modal"
+                           // onClick={() => setDescription(todo.description)}
+                           >
+                               Close
+                           </button>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </>
+   )
 }
+
 
 export default EditTodo
