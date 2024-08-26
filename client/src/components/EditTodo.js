1634745344a1042e6ad625d808511261e5926f4d
Alona Chubenko <aliona.chubenko@gmail.com>
2024-08-26 10:03:57 +0300

    Fixing issues, add handler for closing modal window

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,12 +1,17 @@
-
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
 const EditTodo = ({ todo }) => {
    const [showModal, setShowModal] = useState(false);
-const [description, setDescription] = useState(todo.description)
+   const [description, setDescription] = useState(todo.description)
+   const modalRef = useRef(null) // reference to the modal content
 
-//edit description
+
+
+
+   //edit description
+
+
    const updateDescription = async (e) => {
        e.preventDefault()
        try {
@@ -16,11 +21,32 @@ const [description, setDescription] = useState(todo.description)
                headers: { "Content-Type": "application/json" },
                body: JSON.stringify(body)
            })
-           console.log("resp", response)
+           window.location = "/"
        } catch (err) {
            console.error(err.message)
        }
    }
+
+
+   // Function to close the modal when clicking outside of it
+   const handleClickOutside = (e) => {
+       if (modalRef.current && !modalRef.current.contains(e.target)) {
+           setShowModal(false);
+       }
+   };
+
+
+   // Set up event listener when modal is open
+   useEffect(() => {
+       if (showModal) {
+           document.addEventListener("mousedown", handleClickOutside);
+       } else {
+           document.removeEventListener("mousedown", handleClickOutside);
+       }
+       return () => {
+           document.removeEventListener("mousedown", handleClickOutside); // Cleanup on unmount
+       };
+   }, [showModal]);
 
 
    return (
@@ -36,26 +62,36 @@ const [description, setDescription] = useState(todo.description)
            </button>
 
 
+           {/* id = id10(whatever number we specified) */}
            {showModal && (
-               <div className="modal show d-block" tabIndex="-1" role="dialog" id={`id${todo.todo_id}`}>
+               <div className="modal show d-block" tabIndex="-1" role="dialog" id={`id${todo.todo_id}`}
+
+
+               >
                    <div className="modal-dialog" role="document">
-                       <div className="modal-content">
+                       <div className="modal-content" ref={modalRef}>
                            <div className="modal-header">
                                <h5 className="modal-title">Edit Todo</h5>
                                <button
                                    type="button"
                                    className="close"
-                                   onClick={() => setShowModal(false)}
+                                   onClick={() => {
+                                       setDescription(todo.description);
+                                       setShowModal(false);
+                                   }}
                                >
                                    <span>&times;</span>
                                </button>
                            </div>
+                           {/* Modal body */}
                            <div className="modal-body">
                                <input
                                    type="text"
                                    className="form-control"
-                               value={description}
-                               onChange={e => setDescription(e.target.value)}
+                                   value={description}
+                                   onChange={e => setDescription(e.target.value)}
+
+
                                />
                            </div>
                            <div className="modal-footer">
@@ -69,14 +105,19 @@ const [description, setDescription] = useState(todo.description)
                                <button
                                    type="button"
                                    className="btn btn-danger"
-                                   onClick={() => setShowModal(false)}
+                                   onClick={() => {
+                                       setShowModal(false);
+                                       setDescription(todo.description)
+                                   }}
+
+
                                >
                                    Close
                                </button>
                            </div>
                        </div>
                    </div>
-               </div>
+               </div >
            )}
        </>
    );
